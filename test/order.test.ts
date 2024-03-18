1a61cc80b09523e1f55e0c5e6906e9d60f6c5429
jjbenson85 <jjbenson85@gmail.com>
2024-03-18 13:42:34 +0000

    Create initialization test for Item class

--- test/order.test.ts
+++ test/order.test.ts
@@ -1,14 +1,25 @@
-import { Order } from "../order";
+import { Order, Item } from "../order";
 import { describe, expect, it } from "vitest";
 
 describe("Order", () => {
   it("should have an empty list of items and a shipping address", () => {
-    const order = new Order('123 Fake St');
+    const order = new Order("123 Fake St");
     expect(order.items).toEqual([]);
   });
-  
+
   it("should have a shipping address", () => {
     const order = new Order("123 Fake St");
     expect(order.shippingAddress).toEqual("123 Fake St");
+  });
+});
+
+describe("Item", () => {
+  it("should have a product", () => {
+    const item = new Item("product", 1);
+    expect(item.product).toEqual("product");
+  });
+  it("should have a quantity", () => {
+    const item = new Item("product", 1);
+    expect(item.quantity).toEqual(1);
   });
 });
